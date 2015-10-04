36dd56cc0672a9d632586fb69a608ef7eb786087
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-10-03 18:00:59 -0700

    add references and relationships within db

--- db/migrate/20151004005119_create_homerooms.rb
+++ db/migrate/20151004005119_create_homerooms.rb
@@ -1,7 +1,9 @@
 class CreateHomerooms < ActiveRecord::Migration
   def change
     create_table :homerooms do |t|
-
+      t.references :school, index: true
+      t.references :teacher, index: true
+      t.integer :gradelevel
       t.timestamps null: false
     end
   end
